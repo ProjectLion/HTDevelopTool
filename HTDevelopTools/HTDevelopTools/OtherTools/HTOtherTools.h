d451ab35b32784ddf3b4ee511445b63cd25defa2
mac1 <mac1@162.com>
2017-11-22 16:44:44 +0800

    添加HTOtherTools.h文件

---
+++ HTDevelopTools/HTDevelopTools/OtherTools/HTOtherTools.h
@@ -0,0 +1,13 @@
+//
+//  HTOtherTools.h
+//  HTDevelopTools
+//
+//  Created by HT on 2017/11/22.
+//  Copyright © 2017年 HT. All rights reserved.
+//
+
+#ifndef HTOtherTools_h
+#define HTOtherTools_h
+
+
+#endif /* HTOtherTools_h */
